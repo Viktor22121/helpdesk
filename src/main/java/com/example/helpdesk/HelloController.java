fa640527454fdef673f0618d960a8a41a853c5ba
Viktor22121 <viktorsinaev7@gmail.com>
2026-05-28 09:48:13 +0300

    4

--- src/main/java/com/example/helpdesk/HelloController.java
+++ src/main/java/com/example/helpdesk/HelloController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-    @GetMapping("/api/hello")  // было "/"
+    @GetMapping("/api/hello")
     public String hello() {
         return "Hello, Help Desk!";
     }
